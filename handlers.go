92fdfadaac6ab95e46b0880d5492729d9f6e17a1
Layne <pierce77@bu.edu>
2025-11-22 15:13:52 -0500

    Define routes for getting recent and notable observations by region parameter. Add handler functions that return a closure to allow referencing eBird service instance for response

---
+++ handlers.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	ebs "github.com/sharkbyte79/bird-app/internal/service"
+)
+
+// RecentObservations handles a GET request to a bundle of bird observations
+// from the eBird API according to the given region code.
+func recentObservations(s *ebs.EBirdService) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		region := ctx.Param("region")
+
+		res, err := s.RecentObsByRegion(region, 14, 30)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+
+		ctx.IndentedJSON(http.StatusOK, gin.H{"data": res})
+	}
+}
+
+func notableObservations(s *ebs.EBirdService) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		region := ctx.Param("region")
+
+		res, err := s.NotableObsByRegion(region, 14, 30)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+
+		ctx.IndentedJSON(http.StatusOK, gin.H{"data": res})
+	}
+}
--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	service "github.com/sharkbyte79/bird-app/internal/service"
 )
 
+var eBirdApiKey string
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("failed to load envars from .env")
+	}
+
+	eBirdApiKey = os.Getenv("EBIRD_API_KEY")
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	r := gin.Default()
+
+	// Create one http client for eBird service
+	hc := &http.Client{Timeout: time.Second * 10}
+	s, err := service.NewEBirdService(eBirdApiKey, hc)
+	if err != nil {
+		return
+	}
+
+	r.GET("/observations/:region", recentObservations(s))
+	r.GET("/observations/notable/:region", notableObservations(s))
+
+	r.Run(":8080")
 }
